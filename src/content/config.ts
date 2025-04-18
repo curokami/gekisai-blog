93ba6dfc5c099676af31884289739d3a3f196b43
curokami <berettaxx@gmail.com>
2025-04-18 18:35:11 +0900

    Remove old content.config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,15 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    author: z.string(),
     description: z.string().optional(),
-    pubDate: z.string(), 
+    pubDate: z.string(), // ✅ stringとして受け取る
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
-    cover: z.string().optional(),
-    categories: z.array(z.string()).optional(),
-    tags: z.array(z.string()).optional(),
-    draft: z.boolean().optional(),
   }),
 });
 
 export const collections = {
-  blog, 
+  blog,
 };
